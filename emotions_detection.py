cff91f6de521e734d4a63130bdd8450517ee0ee4
Sejal Maheshwari <maheshwarisejal2201@gmail.com>
2022-05-18 21:07:26 +0530

    Add files via upload

---
+++ emotions_detection.py
@@ -0,0 +1,45 @@
+import os
+import cv2
+import numpy as np
+from keras.models import model_from_json
+from keras.preprocessing import image
+
+model = model_from_json(open("fer.json", "r").read())
+model.load_weights('fer.h5')
+
+
+face_haar_cascade = cv2.CascadeClassifier(
+    'haarcascade_frontalface_default.xml')
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, test_img = cap.read()
+    if not ret:
+        continue
+    gray_img = cv2.cvtColor(test_img, cv2.COLOR_BGR2GRAY)
+
+    faces_detect = face_haar_cascade.detectMultiScale(gray_img, 1.32, 5)
+
+    for (x, y, w, h) in faces_detect:
+        cv2.rectangle(test_img, (x, y), (x+w, y+h), (255, 0, 0), thickness=7)
+        roi_gray = gray_img[y:y+w, x:x+h]
+        roi_gray = cv2.resize(roi_gray, (48, 48))
+        img_pixel = image.img_to_array(roi_gray)
+        img_pixel = np.expand_dims(img_pixel, axis=0)
+        img_pixel /= 255
+        predictions = model.predict(img_pixel)
+        max_index = np.argmax(predictions[0])
+        emotions = ('angry', 'disgust', 'fear', 'happy',
+                    'sad', 'surprise', 'neutral')
+        predicted_emotion = emotions[max_index]
+        cv2.putText(test_img, predicted_emotion, (int(x), int(y)),
+                    cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+
+    resize_img = cv2.resize(test_img, (1000, 700))
+    cv2.imshow('Facial emotion analysis ', resize_img)
+
+    if cv2.waitKey(10) == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows
